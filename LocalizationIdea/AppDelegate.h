c56512fb205dbac249d389836dc028af6711f6c2
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2016-11-06 02:49:44 -0500

    Initial Commit

---
+++ LocalizationIdea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizationIdea
+//
+//  Created by Derrick Ho on 11/6/16.
+//  Copyright © 2016 Derrick Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
